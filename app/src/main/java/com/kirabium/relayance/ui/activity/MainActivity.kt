ffa0ca59ce7d73655a80a16b0911298ed015c8bb
Samuel Lebailly <sam.meusa@gmail.com>
2024-11-27 00:22:42 +0100

    feat(ui/activity): Updating the MainActivity to use her associated viewmodel.

--- app/src/main/java/com/kirabium/relayance/ui/activity/MainActivity.kt
+++ app/src/main/java/com/kirabium/relayance/ui/activity/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.kirabium.relayance.ui.activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.kirabium.relayance.data.DummyData
 import com.kirabium.relayance.databinding.ActivityMainBinding
 import com.kirabium.relayance.ui.adapter.CustomerAdapter
+import com.kirabium.relayance.ui.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var customerAdapter: CustomerAdapter
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setupBinding()
         setupCustomerRecyclerView()
         setupFab()
+        // Observe les données du ViewModel
+        observeCustomers()
     }
 
+    /**
+     * Method to setup the FAB button.
+     */
     private fun setupFab() {
         binding.addCustomerFab.setOnClickListener {
             val intent = Intent(this, AddCustomerActivity::class.java)
@@ -27,9 +38,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Method to setup the RecyclerView.
+     */
     private fun setupCustomerRecyclerView() {
         binding.customerRecyclerView.layoutManager = LinearLayoutManager(this)
-        customerAdapter = CustomerAdapter(DummyData.customers) { customer ->
+        customerAdapter = CustomerAdapter(mutableListOf()) { customer ->
             val intent = Intent(this, DetailActivity::class.java).apply {
                 putExtra(DetailActivity.EXTRA_CUSTOMER_ID, customer.id)
             }
@@ -38,9 +52,26 @@ class MainActivity : AppCompatActivity() {
         binding.customerRecyclerView.adapter = customerAdapter
     }
 
+    /**
+     * Method to setup the content view
+     */
     private fun setupBinding() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
+
+    /**
+     * Method to observe the customers from the ViewModel.
+     */
+    private fun observeCustomers() {
+        // Observer le Flow de clients depuis le ViewModel
+        lifecycleScope.launch {
+            viewModel.customers.collect { customers ->
+                customerAdapter.updateCustomers(customers)
+            }
+        }
+    }
+
+
 }
