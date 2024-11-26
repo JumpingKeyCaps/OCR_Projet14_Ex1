221160ae8578ec29cb16306560e3a860060e6a45
Samuel Lebailly <sam.meusa@gmail.com>
2024-11-27 00:28:46 +0100

    feat(ui/adapter): Add an update method in the CustomerAdapter.

--- app/src/main/java/com/kirabium/relayance/ui/adapter/CustomerAdapter.kt
+++ app/src/main/java/com/kirabium/relayance/ui/adapter/CustomerAdapter.kt
@@ -1,12 +1,13 @@
 package com.kirabium.relayance.ui.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.kirabium.relayance.databinding.CustomerItemBinding
 import com.kirabium.relayance.domain.model.Customer
 
-class CustomerAdapter(private val customers: List<Customer>, private val onClick: (Customer) -> Unit) : RecyclerView.Adapter<CustomerAdapter.CustomerViewHolder>() {
+class CustomerAdapter(private var customers: MutableList<Customer>, private val onClick: (Customer) -> Unit) : RecyclerView.Adapter<CustomerAdapter.CustomerViewHolder>() {
 
     class CustomerViewHolder(private val binding: CustomerItemBinding, val onClick: (Customer) -> Unit) : RecyclerView.ViewHolder(binding.root) {
         private var currentCustomer: Customer? = null
@@ -39,4 +40,15 @@ class CustomerAdapter(private val customers: List<Customer>, private val onClick
     }
 
     override fun getItemCount() = customers.size
+
+    /**
+     * Update the list of customers.
+     * @param newCustomers The new list of customers.
+     */
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateCustomers(newCustomers: List<Customer>) {
+        customers.clear() // Efface l'ancienne liste
+        customers.addAll(newCustomers) // Ajoute les nouveaux clients
+        notifyDataSetChanged() // Notifie l'adaptateur que la liste a changé
+    }
 }
