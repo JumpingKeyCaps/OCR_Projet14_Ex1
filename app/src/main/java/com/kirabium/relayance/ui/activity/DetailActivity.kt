ceef5249726b4428fc6f3bb392068f781be6dfdc
Samuel Lebailly <sam.meusa@gmail.com>
2024-11-27 00:25:08 +0100

    feat(ui/activity): Updating the DetailActivity to use her associated viewmodel.

--- app/src/main/java/com/kirabium/relayance/ui/activity/DetailActivity.kt
+++ app/src/main/java/com/kirabium/relayance/ui/activity/DetailActivity.kt
@@ -1,32 +1,67 @@
 package com.kirabium.relayance.ui.activity
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.kirabium.relayance.R
-import com.kirabium.relayance.data.DummyData
-import com.kirabium.relayance.databinding.ActivityDetailBinding
-import com.kirabium.relayance.extension.DateExt.Companion.toHumanDate
+import androidx.lifecycle.lifecycleScope
 import com.kirabium.relayance.ui.composable.DetailScreen
+import com.kirabium.relayance.ui.viewmodel.DetailViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+/**
+ * Activité des détails du client.
+ */
+@AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_CUSTOMER_ID = "customer_id"
     }
 
+    private val viewModel: DetailViewModel by viewModels()
+
+    /**
+     * Méthode appelée lorsque l'activité est créée.
+     * @param savedInstanceState L'état sauvegardé de l'activité.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupUI()
     }
 
+    /**
+     * Méthode pour configurer l'interface utilisateur.
+     */
     private fun setupUI() {
         val customerId = intent.getIntExtra(EXTRA_CUSTOMER_ID, -1)
-        DummyData.customers.find { it.id == customerId }?.let {
-            setContent {
-                DetailScreen(customer = it) {
-                    onBackPressedDispatcher.onBackPressed()
+
+        if (customerId != -1) {// Vérifie que l'ID est valide
+            // Charge les données via le ViewModel
+            viewModel.getCustomerById(customerId)
+            // Observer les données du client
+            lifecycleScope.launch {
+                viewModel.customer.collect { customer ->
+                    customer?.let {
+                        // Une fois les données reçues, afficher la vue
+                        setContent {
+                            DetailScreen(customer = it) {
+                                onBackPressedDispatcher.onBackPressed()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Observer les erreurs (si présentes)
+        lifecycleScope.launch {
+            viewModel.error.collect { errorMessage ->
+                errorMessage?.let {
+                    // Afficher un Toast ou un dialogue d'erreur
+                    Toast.makeText(this@DetailActivity, it, Toast.LENGTH_LONG).show()
                 }
             }
         }
