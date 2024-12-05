7d3538ba62446e5eb1d028e22ddfcea103140466
Samuel Lebailly <sam.meusa@gmail.com>
2024-12-05 18:58:02 +0100

    feat(ui/activity): Reworking of the DetailActivity.

--- app/src/main/java/com/kirabium/relayance/ui/activity/DetailActivity.kt
+++ app/src/main/java/com/kirabium/relayance/ui/activity/DetailActivity.kt
@@ -1,15 +1,12 @@
 package com.kirabium.relayance.ui.activity
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.kirabium.relayance.ui.composable.DetailScreen
 import com.kirabium.relayance.ui.viewmodel.DetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 /**
  * Activité des détails du client.
@@ -37,33 +34,11 @@ class DetailActivity : AppCompatActivity() {
      */
     private fun setupUI() {
         val customerId = intent.getIntExtra(EXTRA_CUSTOMER_ID, -1)
-
-        if (customerId != -1) {// Vérifie que l'ID est valide
-            // Charge les données via le ViewModel
-            viewModel.getCustomerById(customerId)
-            // Observer les données du client
-            lifecycleScope.launch {
-                viewModel.customer.collect { customer ->
-                    customer?.let {
-                        // Une fois les données reçues, afficher la vue
-                        setContent {
-                            DetailScreen(customer = it) {
-                                onBackPressedDispatcher.onBackPressed()
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // Observer les erreurs (si présentes)
-        lifecycleScope.launch {
-            viewModel.error.collect { errorMessage ->
-                errorMessage?.let {
-                    // Afficher un Toast ou un dialogue d'erreur
-                    Toast.makeText(this@DetailActivity, it, Toast.LENGTH_LONG).show()
-                }
-            }
+        setContent {
+            DetailScreen(
+                customerId = customerId,
+                onBackClick = { onBackPressedDispatcher.onBackPressed() }
+            )
         }
     }
 }
